38a56f6cb59ad535974d2448a7ce066ab5311ea4
furukawasyunngo <furukawasyunngo@gmail.com>
2017-04-19 14:47:25 +0900

    CToriの追加

---
+++ object-practice-2016/CTori.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace object_practice_2016
+{
+    class CTori : CIkimono
+    {
+        private static int count = 0;
+
+        public CTori(string nm) : base(nm)
+        {
+            count++;
+        }
+
+        public static int getCount()
+        {
+            return count;
+        }
+
+        public string action()
+        {
+            return "パタパタ";
+        }
+    }
+}
